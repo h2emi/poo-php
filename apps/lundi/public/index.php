8e24323b70aaa80c382484d1192093e218c12bf7
Emilie FLick <h2e.flick@gmail.com>
2020-04-28 20:28:55 +0200

    creation du rooter

--- apps/lundi/public/index.php
+++ apps/lundi/public/index.php
@@ -1,74 +1,39 @@
- <?php
-
+<?php
 use App\Tools\DevTools;
 use App\Tools\LibsLoader;
 use App\Tools\DatabaseTools;
-
-use App\Models\Plant;
-use App\Models\PlantTools;
-use App\Models\CategoryTools;
 
 $loader = require '../vendor/autoload.php';
 
 //instancier et appeller les librairies externes
 $libsLoader = new LibsLoader();
 $libsLoader->loadLibrairies();
-//instancier un devTool
+
+//instancier un devTools
 $tools = new DevTools();
-//instancier un tool pour pouvoir utiliser la BDD
+
+//instancier un tool pour pouvoir utiliser la BDD (params : credentials pour la BDD)
 $dbTools = new DatabaseTools("mysql", "poo", "root", "root");
 
-//instancier un tool qui fera référence à un model
-$plantTools = new PlantTools($dbTools);
-$categoryTools = new CategoryTools($dbTools);
 
-?>
+/* ligne magique qui affiche les erreurs */
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+$request = $_SERVER['REQUEST_URI'];
+$uri = parse_url($request, PHP_URL_PATH);
 
- <!doctype html>
- <html lang="en">
-
- <head>
-     <meta charset="UTF-8">
-     <meta name="viewport"
-         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-     <title>Document</title>
- </head>
-
- <body>
-     <div class="container">
-
-         <h4>Liste des plantes</h4>
-         <table class="table">
-             <thead class="thead-dark">
-                 <tr>
-                     <th scope="col">Nom</th>
-                     <th scope="col">Categorie</th>
-                     <th scope="col">Price</th>
-                 </tr>
-             </thead>
-             <tbody>
-                 <?php
-                 $plants = $plantTools->getAllPlants();
-                 
-    foreach ($plants as $plant) {?>
-                 <tr>
-                     <td><?= $plant->getName() ?></td>
-                     <td><?php echo $plant->getCategory()->getName() ?></td>
-                     <td><?= $plant->getPrice() ?>€</td>
-
-                 </tr>
-                 <?php }?>
-             </tbody>
-         </table>
-
-
-
-
-     </div>
- </body>
-
- </html>
+switch ($uri) {
+    case '/':
+        require __DIR__ . '/pages/homepage.php';
+        break;
+    case '/article':
+        require __DIR__ . '/pages/article.php';
+        break;
+    case '/newArticle':
+        require __DIR__ . '/pages/newArticle.php';
+        break;
+        
+    default:
+        require __DIR__ . '/pages/homepage.php';
+        break;
+}
